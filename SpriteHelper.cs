f0f00d62de2d91a40dacb23211c1bdc0ec2e6c66
Nguyễn Gia MInh <111077787+ming6464@users.noreply.github.com>
2023-07-24 16:41:51 +0700

    Update SpriteHelper.cs

--- SpriteHelper.cs
+++ SpriteHelper.cs
@@ -13,6 +13,45 @@ public class SpriteHelper
 
         return false;
     }
+    
+    public static bool Contains(Sprite[] sprites, Sprite sprite)
+    {
+        if (sprites == null || sprite == null) return false;
+        foreach (Sprite sp in sprites)
+        {
+            if (CompareSprites(sp, sprite)) return true;
+        }
+
+        return false;
+    }
+    
+    
+
+    public static int IndexOf(List<Sprite> sprites, Sprite sprite)
+    {
+        int index = -1;
+        foreach (var sp in sprites)
+        {
+            index++;
+            if(CompareSprites(sp,sprite)) break;
+        }
+
+        return index;
+    }
+    
+    public static int IndexOf(Sprite[] sprites, Sprite sprite)
+    {
+        int index = -1;
+        foreach (var sp in sprites)
+        {
+            index++;
+            if(CompareSprites(sp,sprite)) break;
+        }
+
+        return index;
+    }
+    
+    
 
     public static bool CompareSprites(Sprite sprite1, Sprite sprite2)
     {
