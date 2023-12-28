5f028a2efdb1873b8f92998e3a4c53e19a5cde8d
Nguyễn Gia MInh <111077787+ming6464@users.noreply.github.com>
2023-12-28 14:24:30 +0700

    Update Singleton.cs

--- Singleton.cs
+++ Singleton.cs
@@ -1,7 +1,56 @@
-﻿namespace RDG
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 {
-    public class Singleton
+    // private static instance
+    static T instance;
+
+    // public static instance used to refer to Singleton (e.g. MyClass.Instance)
+    public static T Instance
+    {
+        get
+        {
+            // if no instance is found, find the first GameObject of type T
+            if (instance == null)
+            {
+                instance = GameObject.FindObjectOfType<T>();
+
+                // if no instance exists in the Scene, create a new GameObject and add the Component T 
+                if (instance == null)
+                {
+                    GameObject singleton = new GameObject(typeof(T).Name);
+                    instance = singleton.AddComponent<T>();
+                }
+            }
+            // return the singleton instance
+            return instance;
+        }
+    }
+
+    public virtual void Awake()
+    {
+        MakeSingleton(true);
+    }
+    public virtual void Start()
     {
         
+    }
+    public virtual void Update()
+    {
+        
+    }
+
+    public void MakeSingleton(bool destroyOnload)
+    {
+        if (instance == null)
+        {
+            instance = this as T;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }
